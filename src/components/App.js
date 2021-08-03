9f6efa4a5802d5401b4491d1cd8d6ba7861b2240
christianotieno <oduorchristian@gmail.com>
2021-08-03 12:37:12 +0300

    Add enzyme and first test for App component

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,22 @@ import FourOFour from './404';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Switch>
-        <Route exact path="/">
-          <AlbumList />
-        </Route>
-        <Route exact path="/album/:albumName">
-          <Album />
-        </Route>
-        <Route exact path="/404">
-          <FourOFour />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <ScrollToTop />
+        <Switch>
+          <Route exact path="/">
+            <AlbumList />
+          </Route>
+          <Route exact path="/album/:albumName">
+            <Album />
+          </Route>
+          <Route exact path="/404">
+            <FourOFour />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
